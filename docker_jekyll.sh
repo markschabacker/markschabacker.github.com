b9f7794b4d2af80a059c27ff8aa490b7ead2fe6f
Mark Schabacker <mark@schaba.com>
2023-06-20 18:19:17 -0400

    updating jekyll

--- docker_jekyll.sh
+++ docker_jekyll.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-docker run --rm --label=jekyll --name=jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll:pages jekyll serve
+#docker run --rm --label=jekyll --name=jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll:pages jekyll serve
+
+docker run --rm \
+  --platform linux/amd64 \
+  --volume="$PWD:/srv/jekyll:Z" \
+  --publish [::1]:4000:4000 \
+  jekyll/jekyll \
+  jekyll serve
